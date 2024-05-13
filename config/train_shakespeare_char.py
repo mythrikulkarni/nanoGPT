c7c646928072439ac9cf7751b995dd397a96e6a9
Mythri Kulkarni <mvkulkarni@ucdavis.edu>
2024-05-13 16:57:02 -0700

    speed up attention module

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -20,8 +20,8 @@ block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
 n_layer = 6
-n_head = 6
-n_embd = 384
+n_head = 8
+n_embd = 32
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
